81f999f287510c2c247a8789ac7159b4bcae74e1
Gokhan <gokhanbilgin5@gmail.com>
2021-07-24 23:17:38 +0300

    Jpa Lesson 05

---
+++ jpa-lesson-05/src/main/java/entities/Product.java
@@ -0,0 +1,50 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private double price;
+
+    @OneToOne(mappedBy = "product")
+    private Detail detail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Detail getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Detail detail) {
+        this.detail = detail;
+    }
+}
